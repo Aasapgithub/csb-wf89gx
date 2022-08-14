744b9f43f6fb3348f1456b19a5e527f7a6bb5e04
Code Sandbox <hello@codesandbox.io>
2022-08-14 12:04:05 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.53b92e913793b103cca628dac20d9cab.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "042fe9155beae495b624b9c54a9b3dcd",
+    "url": "https://aasapgithub.github.io/csb-wf89gx/index.html"
+  },
+  {
+    "revision": "9e8986149fdea944c65a",
+    "url": "https://aasapgithub.github.io/csb-wf89gx/static/js/2.d8b9c863.chunk.js"
+  },
+  {
+    "revision": "374bdbe9d508942b5bf6",
+    "url": "https://aasapgithub.github.io/csb-wf89gx/static/js/main.d7a179dc.chunk.js"
+  },
+  {
+    "revision": "951b1fdaad7a6e7de821",
+    "url": "https://aasapgithub.github.io/csb-wf89gx/static/js/runtime-main.8eecb2c9.js"
+  }
+]);
